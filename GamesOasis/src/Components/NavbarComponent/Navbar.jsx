a2a3444e186ca5a9a11c48887d80924193c0b855
Mohamad-Younes <142875332+Mohamadalyouness@users.noreply.github.com>
2024-03-16 18:52:59 +0200

    Update Navbar.jsx

--- GamesOasis/src/Components/NavbarComponent/Navbar.jsx
+++ GamesOasis/src/Components/NavbarComponent/Navbar.jsx
@@ -20,31 +20,36 @@ export default function Navbar() {
     setUserDataExists(null);
   };
 
+  // Function to handle link click and close the menu
+  const handleLinkClick = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/">
+        <Link to="/" onClick={handleLinkClick}>
           <img src={Logo} alt="Logo" />
         </Link>
       </div>
       <FaBars className="burger-icon" onClick={toggleMenu} />
       {/* Render menu links */}
       <div className={`links ${menuOpen ? "open" : ""}`}>
-        <Link to="/">HOME</Link>
-        <Link to="/CommunityPage">COMMUNITY</Link>
-        <Link to="/GamesPage">DATABASE</Link>
-        <Link to="/AboutUsPage">ABOUT US</Link>
-        <Link to="/ContactUs">CONTACT US</Link>
-           {/* Render login/logout button */}
-      {userDataExists ? (
-        <button className="LoginButton" onClick={logoutFn}>
-          Logout
-        </button>
-      ) : (
-        <Link to="/Login">
-          <button className="LoginButton">Login</button>
-        </Link>
-      )}
+        <Link to="/" onClick={handleLinkClick}>HOME</Link>
+        <Link to="/CommunityPage" onClick={handleLinkClick}>COMMUNITY</Link>
+        <Link to="/GamesPage" onClick={handleLinkClick}>DATABASE</Link>
+        <Link to="/AboutUsPage" onClick={handleLinkClick}>ABOUT US</Link>
+        <Link to="/ContactUs" onClick={handleLinkClick}>CONTACT US</Link>
+        {/* Render login/logout button */}
+        {userDataExists ? (
+          <button className="LoginButton" onClick={logoutFn}>
+            Logout
+          </button>
+        ) : (
+          <Link to="/Login" onClick={handleLinkClick}>
+            <button className="LoginButton">Login</button>
+          </Link>
+        )}
       </div>
       <div className={`overlay ${menuOpen ? "open" : ""}`}></div>
     </nav>
